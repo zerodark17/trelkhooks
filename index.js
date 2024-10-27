755dd0dc6515ee89b182279bc83e5290c7f4a286
oscar <119764740+zerodark17@users.noreply.github.com>
2024-10-27 12:57:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,16 +6,11 @@ const token = '7588166869:AAGroOeWsYbM_QmovwQmf6RvYFZ_maalwI0';
 // Crear una instancia del bot
 const bot = new TelegramBot(token, {polling: true});
 
-// Responder al comando /start
-bot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, '¡Bienvenido! Envíame un mensaje y te responderé.');
-});
 
 // Responder a cualquier mensaje de texto
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   
   // Responder con "Hola" a cualquier mensaje
-  bot.sendMessage(chatId, 'Hola');
+bot.sendMessage(chatId, 'This bot only sends alerts. To interact, please go to @TrelkBot.');
 });
